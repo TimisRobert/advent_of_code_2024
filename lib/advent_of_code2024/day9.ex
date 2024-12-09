359509bbf85f2139396cd1e591c227ea2833bd78
TimisRobert <roberttimis@proton.me>
2024-12-09 23:24:50 +0100

    chore: pt 2

--- lib/advent_of_code2024/day9.ex
+++ lib/advent_of_code2024/day9.ex
@@ -1,15 +1,17 @@
 defmodule AdventOfCode2024.Day9 do
   use AdventOfCode2024
 
-  defp create_files({0, _}, acc), do: acc
+  defp create_files(list, acc \\ {[], 0})
 
-  defp create_files({size, idx}, {list, id}) when rem(idx, 2) == 0 do
-    {List.duplicate(id, size) ++ list, id + 1}
+  defp create_files([{size, idx} | t], {list, id}) when rem(idx, 2) == 0 do
+    create_files(t, {List.duplicate(id, size) ++ list, id + 1})
   end
 
-  defp create_files({size, _}, {list, id}) do
-    {List.duplicate(nil, size) ++ list, id}
+  defp create_files([{size, _} | t], {list, id}) do
+    create_files(t, {List.duplicate(nil, size) ++ list, id})
   end
+
+  defp create_files([], {list, _}), do: Enum.reverse(list)
 
   defp parse_input(input) do
     input
@@ -17,64 +19,59 @@ defmodule AdventOfCode2024.Day9 do
     |> String.graphemes()
     |> Enum.map(&String.to_integer/1)
     |> Enum.with_index()
-    |> Enum.reduce({[], 0}, &create_files/2)
-    |> then(&elem(&1, 0))
+    |> create_files()
   end
 
-  defp reorder_files(list, tot), do: reorder_files(Enum.reverse(list), list, [], tot)
-  defp reorder_files(_, _, acc, 0), do: Enum.reverse(acc)
-  defp reorder_files(t, [nil | l], acc, tot), do: reorder_files(t, l, acc, tot)
-  defp reorder_files([nil | t], [f | l], acc, tot), do: reorder_files(t, l, [f | acc], tot - 1)
-  defp reorder_files([h | t], l, acc, tot), do: reorder_files(t, l, [h | acc], tot - 1)
+  defp reorder_files(list), do: reorder_files(list, Enum.reverse(list), {[], 0, length(list)})
+  defp reorder_files(_, _, {list, one, one}), do: Enum.reverse(list)
+  defp reorder_files(t, [nil | l], {acc, one, two}), do: reorder_files(t, l, {acc, one, two - 1})
+
+  defp reorder_files([nil | t], [f | l], {acc, one, two}),
+    do: reorder_files(t, l, {[f | acc], one + 1, two - 1})
+
+  defp reorder_files([h | t], l, {acc, one, two}),
+    do: reorder_files(t, l, {[h | acc], one + 1, two})
 
   def part_one(input) do
-    list = parse_input(input)
-    tot = Enum.count(list, & &1)
-
-    list
-    |> reorder_files(tot)
+    input
+    |> parse_input()
+    |> reorder_files()
     |> Enum.with_index()
     |> Enum.map(&Tuple.product/1)
     |> Enum.sum()
   end
 
-  defp create_blocks(list), do: create_blocks(list, [])
-  defp create_blocks([h | t], [[h | _] = c | l]), do: create_blocks(t, [[h | c] | l])
-  defp create_blocks([nil | t], acc), do: create_blocks(t, acc)
-  defp create_blocks([h | t], l), do: create_blocks(t, [[h] | l])
-  defp create_blocks([], acc), do: Enum.reverse(acc)
-
-  defp skip_nils(list), do: skip_nils(list, 0)
-  defp skip_nils([nil | r], acc), do: skip_nils(r, acc + 1)
-  defp skip_nils(list, acc), do: {list, acc}
-
-  defp reorder_blocks(list, blocks), do: reorder_blocks(Enum.reverse(list), blocks, [], nil)
-
-  defp reorder_blocks([nil | _] = list, [f | l] = b, acc, nil) do
-    if hd(f) in acc do
-      reorder_blocks(Enum.reverse(acc) ++ list, l, [], nil)
-    else
-      {list, nils} = skip_nils(list)
-
-      if length(f) <= nils do
-        reorder_blocks(list, l, List.duplicate(nil, nils - length(f)) ++ f ++ acc, List.first(f))
-      else
-        reorder_blocks(list, b, List.duplicate(nil, nils) ++ acc, nil)
-      end
-    end
-  end
-
-  defp reorder_blocks([], [_ | l], acc, _), do: reorder_blocks(Enum.reverse(acc), l, [], nil)
-  defp reorder_blocks([h | t], b, acc, h), do: reorder_blocks(t, b, [nil | acc], h)
-  defp reorder_blocks([h | t], b, acc, del), do: reorder_blocks(t, b, [h | acc], del)
-  defp reorder_blocks(_, [], acc, _), do: Enum.reverse(acc)
-
   def part_two(input) do
-    list = parse_input(input)
-    blocks = create_blocks(list)
+    chunks =
+      input
+      |> parse_input()
+      |> Enum.chunk_by(& &1)
+      |> Enum.with_index()
 
-    list
-    |> reorder_blocks(blocks)
+    chunks
+    |> Enum.filter(fn {chunk, _} -> Enum.all?(chunk) end)
+    |> Enum.reverse()
+    |> Enum.reduce(chunks, fn {block, block_idx}, chunks ->
+      chunks
+      |> Enum.find(fn {chunk, _} -> Enum.count(chunk, &is_nil/1) >= length(block) end)
+      |> case do
+        {_, chunk_idx} when chunk_idx < block_idx ->
+          chunks
+          |> List.update_at(chunk_idx, fn {chunk, idx} ->
+            chunk
+            |> Enum.reduce({block, []}, fn
+              nil, {[h | t], acc} -> {t, [h | acc]}
+              elem, {block, acc} -> {block, [elem | acc]}
+            end)
+            |> then(&{Enum.reverse(elem(&1, 1)), idx})
+          end)
+          |> List.replace_at(block_idx, {List.duplicate(nil, length(block)), block_idx})
+
+        _ ->
+          chunks
+      end
+    end)
+    |> Enum.flat_map(&elem(&1, 0))
     |> Enum.with_index()
     |> Enum.reject(&is_nil(elem(&1, 0)))
     |> Enum.map(&Tuple.product/1)
