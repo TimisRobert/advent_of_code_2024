f74085db42cd0254e57f3b4b67ac7cc3b8637a57
TimisRobert <roberttimis@proton.me>
2024-12-12 19:26:33 +0100

    chore: mix generator

--- lib/mix/tasks/generate_day.ex
+++ lib/mix/tasks/generate_day.ex
@@ -1,30 +1,17 @@
 defmodule Mix.Tasks.GenerateDay do
   use Mix.Task
+  alias Mix.Generator
 
   @impl true
   def run([day]) do
     app_dir = Application.app_dir(:advent_of_code_2024)
 
-    app_dir
-    |> Path.join("/priv/day#{day}.txt")
-    |> File.write!("")
+    Generator.create_file("priv/day#{day}.txt", "")
 
-    day_template =
-      app_dir
-      |> Path.join("/priv/day_template.eex")
-      |> EEx.eval_file(day: day)
+    template = Path.join(app_dir, "priv/day_template.eex")
+    test_template = Path.join(app_dir, "priv/day_template_test.eex")
 
-    day_test_template =
-      app_dir
-      |> Path.join("/priv/day_template_test.eex")
-      |> EEx.eval_file(day: day)
-
-    app_dir
-    |> Path.join("../../../../lib/advent_of_code2024/day#{day}.ex")
-    |> File.write!(day_template)
-
-    app_dir
-    |> Path.join("../../../../test/advent_of_code2024/day#{day}_test.exs")
-    |> File.write!(day_test_template)
+    Generator.copy_template(template, "lib/advent_of_code2024/day#{day}.ex", day: day)
+    Generator.copy_template(test_template, "test/advent_of_code2024/day#{day}_test.exs", day: day)
   end
 end
